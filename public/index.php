30830ff8d9e26a9da69fc5a78a946ab7434021d5
Uriel <devuri@users.noreply.github.com>
2024-08-01 12:34:29 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+
 define( 'WP_USE_THEMES', true );
 
 if ( file_exists( __DIR__ . '/wp/wp-blog-header.php' ) ) {
