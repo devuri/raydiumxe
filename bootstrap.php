0edd13fa0261327351d4a3e13d89aebfd86a9383
Uriel <devuri@users.noreply.github.com>
2024-08-01 12:37:41 -0500

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -2,45 +2,16 @@
 
 use WPframework\Component\Kernel;
 
-/*
- * This is the bootstrap file for the web application.
- *
- * It loads the necessary files and sets up the environment for the application to run.
- * This includes initializing the Composer autoloader, which is used to load classes and packages.
- */
 if (file_exists(\dirname(__FILE__) . "/vendor/autoload.php")) {
     require_once \dirname(__FILE__) . "/vendor/autoload.php";
 } else {
-    exit("Cant find the vendor autoload file.");
+    exit("Can't find the vendor autoload file.");
 }
 
-/**
- * Start and bootstrap the web application.
- *
- * @var Kernel
- */
 $raydium_http = http_component_kernel(__DIR__);
 
-/*
- * Load constant overrides.
- *
- * This will load constant values that override constants defined in setup.
- */
 $raydium_http->overrides();
 
-/*
- * Configuration settings for your web application.
- *
- * We recommend using the .env file to set these values.
- * The possible values are: 'debug', 'development', 'staging', 'production', or 'secure'.
- * The web application will use either the value of WP_ENVIRONMENT_TYPE or 'production'.
- */
 $raydium_http->init();
 
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
- */
 $table_prefix = env("DB_PREFIX");
