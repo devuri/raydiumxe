81ce223dfd046c23080e91109cd5611d53c0cdd8
Uriel <devuri@users.noreply.github.com>
2024-09-10 01:23:38 -0500

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,11 +1,24 @@
 <?php
 
+use WPframework\Component\App;
+
+// This is the bootstrap file for the web application.
+if ( file_exists( \dirname( __FILE__ ) . '/../vendor/autoload.php' ) ) {
+    require_once \dirname( __FILE__ ) . '/../vendor/autoload.php';
+} else {
+    exit( 'Cant find the vendor autoload file.' );
+}
+
+
+if ( ! defined( 'RAYDIUM_ENVIRONMENT_TYPE' ) ) {
+	define( 'RAYDIUM_ENVIRONMENT_TYPE', null );
+}
 
 /* That's all, stop editing! Happy publishing. */
 
-if ( file_exists(__DIR__ . '/../bootstrap.php') ) {
-	require_once __DIR__ . '/../bootstrap.php';
-}
+App::init( dirname( __DIR__ ) )->overrides()->init( RAYDIUM_ENVIRONMENT_TYPE );
+
+$table_prefix = env( 'DB_PREFIX' );
 
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
