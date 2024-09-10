9a1aa7b722db5381e1639481d78a51c9999e1c9d
Uriel <devuri@users.noreply.github.com>
2024-09-10 10:08:07 -0500

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -2,26 +2,28 @@
 
 use WPframework\Component\App;
 
-// This is the bootstrap file for the web application.
-if ( file_exists( \dirname( __FILE__ ) . '/../vendor/autoload.php' ) ) {
-    require_once \dirname( __FILE__ ) . '/../vendor/autoload.php';
+if ( file_exists( dirname( __DIR__ ) . '/vendor/autoload.php' ) ) {
+    require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 } else {
-    exit( 'Cant find the vendor autoload file.' );
+    exit( 'Cannot find the vendor autoload file.' );
 }
-
 
 if ( ! defined( 'RAYDIUM_ENVIRONMENT_TYPE' ) ) {
-	define( 'RAYDIUM_ENVIRONMENT_TYPE', null );
+    define( 'RAYDIUM_ENVIRONMENT_TYPE', null );
 }
+
 
 /* That's all, stop editing! Happy publishing. */
 
-App::init( dirname( __DIR__ ) )->overrides()->init( RAYDIUM_ENVIRONMENT_TYPE );
+App::init( dirname( __DIR__ ) )
+    ->overrides()
+    ->init( RAYDIUM_ENVIRONMENT_TYPE );
 
 $table_prefix = env( 'DB_PREFIX' );
 
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+    define( 'ABSPATH', __DIR__ . '/' );
 }
+
 
 require_once ABSPATH . 'wp-settings.php';
