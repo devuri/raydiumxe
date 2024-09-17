76da9f01653fbffbfe14ef1727ac82cbe9e7cd01
Uriel <devuri@users.noreply.github.com>
2024-09-17 12:57:19 -0500

    Update wp-config.php

--- public/wp-config.php
+++ public/wp-config.php
@@ -2,31 +2,26 @@
 
 use WPframework\App;
 
-if ( file_exists( dirname( __DIR__ ) . '/vendor/autoload.php' ) ) {
-    require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    require_once dirname(__DIR__) . '/vendor/autoload.php';
 } else {
-    exit( 'Cannot find the vendor autoload file.' );
-}
-
-if ( ! defined( 'RAYDIUM_ENVIRONMENT_TYPE' ) ) {
-    define( 'RAYDIUM_ENVIRONMENT_TYPE', null );
+    exit('Cannot find the vendor autoload file.');
 }
 
 
-// define('WP_DEBUG', true);
-// define('WP_DEBUG_LOG', true);
-// define('WP_DEBUG_DISPLAY', true);
+if (! defined('RAYDIUM_ENVIRONMENT_TYPE')) {
+    define('RAYDIUM_ENVIRONMENT_TYPE', null);
+}
 
 
 /* That's all, stop editing! Happy publishing. */
 
-App::init( dirname( __DIR__ ) )->app( RAYDIUM_ENVIRONMENT_TYPE );
+App::init(dirname(__DIR__))->app(RAYDIUM_ENVIRONMENT_TYPE);
 
-$table_prefix = env( 'DB_PREFIX' );
+$table_prefix = env('DB_PREFIX');
 
-if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
+if (! defined('ABSPATH')) {
+    define('ABSPATH', __DIR__ . '/');
 }
-
 
 require_once ABSPATH . 'wp-settings.php';
