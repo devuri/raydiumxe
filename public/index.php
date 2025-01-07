d02bd44227eb86c191d018572118d429e67795b3
uriel <devuri@users.noreply.github.com>
2025-01-07 11:34:23 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
 <?php
 
+/**
+ * Define whether to use WordPress themes.
+ */
 define( 'WP_USE_THEMES', true );
 
-if ( file_exists( __DIR__ . '/wp/wp-blog-header.php' ) ) {
-	require __DIR__ . '/wp/wp-blog-header.php';
+/**
+ * Define lightweight framework mode.
+ */
+define( 'HYBRIDX', false );
+
+/**
+ * Bootstrap the application.
+ */
+if ( defined( 'HYBRIDX' ) && true === constant( 'HYBRIDX' ) ) {
+    require __DIR__ . '/wp-config.php';
+} elseif ( file_exists( __DIR__ . '/wp/wp-blog-header.php' ) ) {
+    require __DIR__ . '/wp/wp-blog-header.php';
 } else {
-    exit("Looks like the framework is not setup, run setup or composer install.");
+    exit( "Looks like the framework is not set up. Run setup or composer install." );
 }
